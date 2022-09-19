ab90a729f9aeb5835253ce3f42b50a71a1f1386d
bilal karahan <bilalkarahan309@gmail.com>
2022-09-19 16:41:57 +0300

    1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner input= new Scanner(System.in);
+        System.out.println("Üssü alınacak sayıyı giriniz:");
+        int x= input.nextInt();
+        System.out.println("Üssünü alınız:");
+        int y= input.nextInt();
+        int total=1;
+
+        for(int i=1;i<=y;i++){
+            total*=x;
+        }
+        System.out.println("CEVAP:"+total);
+
+
+
+
     }
 }
